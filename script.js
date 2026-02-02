531d0b3d99d09bc099866123374a1a85e7e345fe
Ada1234ma <thompsonada1998@gmail.com>
2026-02-02 12:29:19 +0100

    assigment one

---
+++ script.js
@@ -0,0 +1,26 @@
+// java variables
+const lastname ="Thompson";
+const firstname ="Ada";
+const age =20;
+const height =6.9;
+let weight =60;
+const country ="Nigeria";
+let address ="Alakahia";
+let gender ="female";
+
+console.log(lastname)
+console.log(firstname)
+console.log(age)
+console.log(height)
+console.log(weight)
+console.log(country)
+console.log(address)
+console.log(gender)
+
+
+//  concatenation- combining two or more strings together
+// const firstname = "ADA"
+// const lastname ="THOMPSON"
+// const age = 20
+// const country ="Nigeria"
+// const fullaname = "Hello good morning, my name is $firtname $lastname $age and I'm $age old from $country"
