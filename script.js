2fa34a6aa69a8a7e9e0615107fc53c8d69036613
Ada1234ma <thompsonada1998@gmail.com>
2026-02-04 00:34:34 +0100

    assigbment two

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 // java variables
-const lastname ="Thompson";
-const firstname ="Ada";
-const age =20;
-const height =6.9;
-let weight =60;
-const country ="Nigeria";
-let address ="Alakahia";
-let gender ="female";
+// const lastname ="Thompson";
+// const firstname ="Ada";
+// const age =20;
+// const height =6.9;
+// let weight =60;
+// const country ="Nigeria";
+// let address ="Alakahia";
+// let gender ="female";
 
-console.log(lastname)
-console.log(firstname)
-console.log(age)
-console.log(height)
-console.log(weight)
-console.log(country)
-console.log(address)
-console.log(gender)
+// console.log(lastname)
+// console.log(firstname)
+// console.log(age)
+// console.log(height)
+// console.log(weight)
+// console.log(country)
+// console.log(address)
+// console.log(gender)
 
 
 //  concatenation- combining two or more strings together
-// const firstname = "ADA"
-// const lastname ="THOMPSON"
-// const age = 20
-// const country ="Nigeria"
-// const fullaname = "Hello good morning, my name is $firtname $lastname $age and I'm $age old from $country"
+// const firstname ="ADA";
+// const lastname ="THOMPSON";
+// const age =20;
+// const country ="Nigeria";
+// const fullname = `Hello good morning, my name is ${firstname} ${lastname} and I'm ${age} old from ${country}`;
+// console.log(fullname);
+
+//assignment 
+//write a program that calculates the area of a rectangle given its length and width using assignment operators.
+let length = 2;
+const width = 4;
+const area = length * width;
+console.log(area);
+
+// //write a program that calculates the area of a circle given its radius using assignment and arithmetic operators.
+const pi = 3.14;
+let r = 2;
+const areaOfCircle = pi * r**r;
+console.log(areaOfCircle);
+
+// //write a program that converts temperature from Celsius to Fahrenheit using assignment and arithmetic operators.
+let celsius = 27;
+const fahrenheit = (celsius * 9/5) + 32;
+console.log(fahrenheit);
